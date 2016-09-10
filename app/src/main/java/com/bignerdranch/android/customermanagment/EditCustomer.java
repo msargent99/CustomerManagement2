d0580ff14bcfbcbba4919451484a18064ba01d4f
msargent99 <jcmje5@live.com>
2016-09-09 22:43:19 -0400

    Assignment 3 revisions commit

--- app/src/main/java/com/bignerdranch/android/customermanagment/EditCustomer.java
+++ app/src/main/java/com/bignerdranch/android/customermanagment/EditCustomer.java
@@ -5,12 +5,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MenuActivity extends AppCompatActivity {
+public class EditCustomer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_edit_customer);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
