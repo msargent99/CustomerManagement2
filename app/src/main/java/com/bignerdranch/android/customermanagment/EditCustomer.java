f3c3046b26c365b452bf9eae54ed6ba5373169ff
msargent99 <jcmje5@live.com>
2016-09-28 20:48:39 -0400

    Assignment 5

--- app/src/main/java/com/bignerdranch/android/customermanagment/EditCustomer.java
+++ app/src/main/java/com/bignerdranch/android/customermanagment/EditCustomer.java
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.customermanagment;
 
+import android.content.ContentValues;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditCustomer extends AppCompatActivity {
 
@@ -17,8 +20,16 @@ public class EditCustomer extends AppCompatActivity {
 
         if(fragment == null){
             fragment = new User();
-            fm.beginTransaction ()
-                    .add(R.id.fragment_container, fragment);
+            fm.beginTransaction ().add(R.id.fragment_container, fragment).commit();
         }
+
+        Button  mButtonSubmitCustomer = (Button) findViewById(R.id.button_submit_customer);
+        mButtonSubmitCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+                values.put(myDatabase.Cols.KEY_GOLD_HOARD_NAME_COLUMN, R.id.textbox_customer_name);
+            }
+        });
     }
 }
--- app/src/main/java/com/bignerdranch/android/customermanagment/ViewActivity.java
+++ app/src/main/java/com/bignerdranch/android/customermanagment/ViewActivity.java
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.customermanagment;
 
+import android.content.ContentValues;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ViewActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +20,12 @@ public class ViewActivity extends AppCompatActivity {
 
         if(fragment == null){
             fragment = new User();
-            fm.beginTransaction ()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
+            fm.beginTransaction ().add(R.id.fragment_container, fragment).commit();
         }
+        ContentValues values = new ContentValues();
+        ContentValues names = values.get(db.KEY_GOLD_HOARD_NAME_COLUMN);
+
+        EditText e = (EditText) findViewById (R.id.names);
+        e.setText(names.toString());
     }
 }
